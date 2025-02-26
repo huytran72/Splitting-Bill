fcc448cdaa98ed608d142e40b2baf3311d279047
Huy Tran <baohuy82.th@gmail.com>
2025-02-25 23:59:37 -0800

    update

--- splitting_bill/src/App.js
+++ splitting_bill/src/App.js
@@ -45,7 +45,7 @@ function Friend({ friend }) {
   return (
     <li>
       <img src={friend.image} alt={friend.name} />
-      <h3>{friend.name}</h3>
+      <h2>{friend.name}</h2>
     </li>
   );
 }
