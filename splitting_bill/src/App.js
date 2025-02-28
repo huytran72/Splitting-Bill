4f4e63d36a8ccb77cfee4c5ef853101b0d294504
Huy Tran <baohuy82.th@gmail.com>
2025-02-27 21:57:58 -0800

    Update App.js

--- splitting_bill/src/App.js
+++ splitting_bill/src/App.js
@@ -24,6 +24,7 @@ export default function App() {
     <div className="app">
       <div className="sidebar">
         <FriendList />
+        <FormAddFriend />
         <Button>Add Friend</Button>
       </div>
     </div>
