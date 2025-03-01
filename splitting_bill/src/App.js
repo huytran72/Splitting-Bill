12d185aabe16c15c054e6a29525f65e467dfc893
Huy Tran <baohuy82.th@gmail.com>
2025-02-28 23:48:58 -0800

    Update App.js

--- splitting_bill/src/App.js
+++ splitting_bill/src/App.js
@@ -91,7 +91,7 @@ function FormAddFriend() {
 function FormSplitBill() {
   return (
     <form className="form-split-bill">
-      <h2>Split a bill with </h2>
+      <h2>Split a bill with X </h2>
 
       <label>💰 Bill value</label>
       <input type="text" />
