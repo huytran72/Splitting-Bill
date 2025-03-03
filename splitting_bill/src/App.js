d53bf5a5c4622ea08e23a4f77848c22a4f8ff9e8
Huy Tran <baohuy82.th@gmail.com>
2025-03-03 14:52:18 -0800

    Update App.js

--- splitting_bill/src/App.js
+++ splitting_bill/src/App.js
@@ -32,6 +32,7 @@ function Button({ children, onClick }) {
 export default function App() {
   const [friends, setFriends] = useState(initialFriends);
   const [showAddFriend, setShowAddFriend] = useState(true);
+  const [selectedFriend, setSelectedFriend] = useState(null);
 
   function handleShowAddFriend() {
     setShowAddFriend(!showAddFriend);
