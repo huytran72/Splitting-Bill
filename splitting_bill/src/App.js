0162588d389fe9bcd95e1174bc0626ab760ac8fa
Huy Tran <baohuy82.th@gmail.com>
2025-03-05 23:22:55 -0800

    Update App.js

--- splitting_bill/src/App.js
+++ splitting_bill/src/App.js
@@ -44,7 +44,8 @@ export default function App() {
   }
 
   function handleSelection(friend) {
-    setSelectedFriend(friend);
+    // setSelectedFriend(friend);
+    setSelectedFriend((selected) => {});
   }
 
   return (
